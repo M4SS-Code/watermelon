3f7a5b0d8e58ed0872ccd13572d8256668405785
Paolo Barbolini <paolo.barbolini@m4ss.net>
2025-02-08 17:22:22 +0100

    docs: fix typo on `BoxFuture`

--- watermelon/src/future.rs
+++ watermelon/src/future.rs
@@ -1,4 +1,4 @@
 use std::{future::Future, pin::Pin};
 
-/// An alternative to [`future_core::future::BoxFuture`] that is also `Sync`
+/// An alternative to [`futures_core::future::BoxFuture`] that is also `Sync`
 pub(crate) type BoxFuture<'a, T> = Pin<Box<dyn Future<Output = T> + Send + Sync + 'a>>;
