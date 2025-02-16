f59aadfedbc491a15cb30b5bc4a81420cde00e8c
Paolo Barbolini <paolo.barbolini@m4ss.net>
2025-02-16 16:17:45 +0100

    test: replace `futures_util::task::ArcWake` with `std::task::Wake`

--- watermelon/src/tests.rs
+++ watermelon/src/tests.rs
@@ -1,6 +1,7 @@
-use std::{sync::Arc, task::Waker};
-
-use futures_util::task::ArcWake;
+use std::{
+    sync::Arc,
+    task::{Wake, Waker},
+};
 
 use crate::util::atomic::{AtomicUsize, Ordering};
 
@@ -10,7 +11,7 @@ pub(crate) struct FlagWaker(AtomicUsize);
 impl FlagWaker {
     pub(crate) fn new() -> (Arc<Self>, Waker) {
         let this = Arc::new(Self(AtomicUsize::new(0)));
-        let waker = futures_util::task::waker(Arc::clone(&this));
+        let waker = Waker::from(Arc::clone(&this));
         (this, waker)
     }
 
@@ -19,8 +20,8 @@ impl FlagWaker {
     }
 }
 
-impl ArcWake for FlagWaker {
-    fn wake_by_ref(arc_self: &Arc<Self>) {
-        arc_self.0.fetch_add(1, Ordering::AcqRel);
+impl Wake for FlagWaker {
+    fn wake(self: Arc<Self>) {
+        self.0.fetch_add(1, Ordering::AcqRel);
     }
 }
