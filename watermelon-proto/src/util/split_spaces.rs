b73d573d2b60b974be1def2ec93e43d0a45aa66a
Paolo Barbolini <paolo.barbolini@m4ss.net>
2025-02-16 12:09:09 +0100

    refactor(proto): remove intermediate array from `split_spaces`

--- watermelon-proto/src/util/split_spaces.rs
+++ watermelon-proto/src/util/split_spaces.rs
@@ -1,31 +1,26 @@
-use core::array;
+use core::{iter, mem};
 
 use bytes::{Buf, Bytes};
 
 pub(crate) fn split_spaces(mut bytes: Bytes) -> impl Iterator<Item = Bytes> {
-    let mut chunks = array::from_fn::<_, 6, _>(|_| Bytes::new());
-    let mut found = 0;
+    iter::from_fn(move || {
+        if bytes.is_empty() {
+            return None;
+        }
 
-    for chunk in &mut chunks {
         let Some(i) = memchr::memchr2(b' ', b'\t', &bytes) else {
-            if !bytes.is_empty() {
-                *chunk = bytes;
-                found += 1;
-            }
-            break;
+            return Some(mem::take(&mut bytes));
         };
 
-        *chunk = bytes.split_to(i);
-        found += 1;
+        let chunk = bytes.split_to(i);
 
         let spaces = bytes
             .iter()
             .take_while(|b| matches!(b, b' ' | b'\t'))
             .count();
         bytes.advance(spaces);
-    }
-
-    chunks.into_iter().take(found)
+        Some(chunk)
+    })
 }
 
 #[cfg(test)]
