644ac55951691a18dcbf01ea54d22191f1e1e6f2
Paolo Barbolini <paolo.barbolini@m4ss.net>
2025-02-19 11:03:55 +0100

    feat(nkeys): add missing public re-export to `Signature`

--- watermelon-nkeys/src/lib.rs
+++ watermelon-nkeys/src/lib.rs
@@ -1,6 +1,6 @@
 #![forbid(unsafe_code)]
 
-pub use self::seed::{KeyPair, KeyPairFromSeedError, PublicKey};
+pub use self::seed::{KeyPair, KeyPairFromSeedError, PublicKey, Signature};
 
 mod crc;
 mod seed;
